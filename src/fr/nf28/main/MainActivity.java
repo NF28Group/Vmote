d915f70c6b809e9ea24dcd4e330b4d640290ecab
Nicolas Biehler <biehler.nicolas@gmail.com>
2013-05-21 15:48:54 +0200

    test

--- src/fr/nf28/main/MainActivity.java
+++ src/fr/nf28/main/MainActivity.java
@@ -1,15 +1,21 @@
 package fr.nf28.main;
 
+import com.actionbarsherlock.sample.styled.ActionBar;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final ActionBar ab = getSupportActionBar();
     }
 
     @Override
