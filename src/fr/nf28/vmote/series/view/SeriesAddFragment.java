2063fbd1ec4075e9573150dd7cce86c714aec91d
Nicolas Fortin <nicolas.fortin37@gmail.com>
2013-06-05 17:49:30 +0200

    Fermeture clavier dans champ recherche

--- src/fr/nf28/vmote/series/view/SeriesAddFragment.java
+++ src/fr/nf28/vmote/series/view/SeriesAddFragment.java
@@ -7,10 +7,12 @@ import fr.nf28.vmote.R;
 import fr.nf28.vmote.series.adapter.TvShowSearchAdapter;
 import fr.nf28.vmote.series.model.SeriesModel;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
@@ -18,6 +20,7 @@ import android.widget.SearchView.OnQueryTextListener;
 
 public class SeriesAddFragment extends AbstractSeriesFragment {
 
+	private Context that;
 	private View rootView;
 	private SeriesModel model;
 
@@ -30,13 +33,14 @@ public class SeriesAddFragment extends AbstractSeriesFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.that = getActivity();
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		rootView = inflater.inflate(R.layout.fragment_series_add_layout, container, false);
-		model = new SeriesModel(getActivity());
+		model = new SeriesModel(that);
 
 		//searchInput = (EditText) rootView.findViewById(R.id.search_input);
 		listResult = (ListView) rootView.findViewById(R.id.list_result);
@@ -53,8 +57,8 @@ public class SeriesAddFragment extends AbstractSeriesFragment {
 			public boolean onQueryTextSubmit(String query) {
 				String searchName = searchInput.getQuery().toString();
 				model.searchSeries(searchName);
-				//InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-				//imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+				InputMethodManager imm = (InputMethodManager)that.getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
 				return true;
 			}
 		});
